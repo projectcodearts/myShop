1f8201c1541d775e1936a5e9dab00fa4d474a5c1
rahulyadav1986 <rahullooksforward@gmail.com>
2020-10-28 20:10:47 +0530

    all are updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,105 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'entrypoint',
+    loadChildren: () => import('./all-pages/entrypoint/entrypoint.module').then( m => m.EntrypointPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'entrypoint',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./all-pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./all-pages/forgotpass/forgotpass.module').then( m => m.ForgotpassPageModule)
+  },
+  {
+    path: 'verify-otp',
+    loadChildren: () => import('./all-pages/verify-with-otp/verify-with-otp.module').then( m => m.VerifyWithOtpPageModule)
+  },
+  {
+    path: 'update-password',
+    loadChildren: () => import('./all-pages/update-password/update-password.module').then( m => m.UpdatePasswordPageModule)
+  },
+  {
+    path: 'pasword-reset',
+    loadChildren: () => import('./all-pages/reset-pass-message/reset-pass-message.module').then( m => m.ResetPassMessagePageModule)
+  },
+  {
+    path: 'auth-signup',
+    loadChildren: () => import('./all-pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'verify-otp-register',
+    loadChildren: () => import('./all-pages/verify-otp-register/verify-otp-register.module').then( m => m.VerifyOtpRegisterPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./all-pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'shop',
+    loadChildren: () => import('./all-pages/shop/shop.module').then( m => m.ShopPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./all-pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./all-pages/categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'sub-category',
+    loadChildren: () => import('./all-pages/sub-category/sub-category.module').then( m => m.SubCategoryPageModule)
+  },
+  {
+    path: 'profile-setting',
+    loadChildren: () => import('./all-pages/profile-setting/profile-setting.module').then( m => m.ProfileSettingPageModule)
+  },
+  {
+    path: 'user-account',
+    loadChildren: () => import('./all-pages/user-account/user-account.module').then( m => m.UserAccountPageModule)
+  },
+  {
+    path: 'business-details',
+    loadChildren: () => import('./all-pages/business-details/business-details.module').then( m => m.BusinessDetailsPageModule)
+  },
+  {
+    path: 'account-settings',
+    loadChildren: () => import('./all-pages/account-settings/account-settings.module').then( m => m.AccountSettingsPageModule)
+  },
+  {
+    path: 'select-address',
+    loadChildren: () => import('./all-pages/select-address/select-address.module').then( m => m.SelectAddressPageModule)
+  },
+  {
+    path: 'add-new-address',
+    loadChildren: () => import('./all-pages/add-new-address/add-new-address.module').then( m => m.AddNewAddressPageModule)
+  },
+  {
+    path: 'your-orders',
+    loadChildren: () => import('./all-pages/your-orders/your-orders.module').then( m => m.YourOrdersPageModule)
+  },
+  {
+    path: 'setup-profile',
+    loadChildren: () => import('./all-pages/setup-profile/setup-profile.module').then( m => m.SetupProfilePageModule)
+  },
+  {
+    path: 'about-us',
+    loadChildren: () => import('./all-pages/about-us/about-us.module').then( m => m.AboutUsPageModule)
+  },
+ 
+ 
+  
+  
+  
+  
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './all-components/footer/footer.component';
+import { HeaderComponent } from './all-components/header/header.component';
+import { MenuComponent } from './all-components/header/menu/menu.component';
+import { MenuSocialComponent } from './all-components/header/menu-social/menu-social.component';
+import { ProfileContentComponent } from './all-components/header/profile-content/profile-content.component';
+import { MainMenuComponent } from './all-components/header/main-menu/main-menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, MenuComponent, MenuSocialComponent, ProfileContentComponent, MainMenuComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
